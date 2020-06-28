efeb077cc330d54a24161a242de1f9a8830c6ba6
Sebastian Berning <hunter.Vasitas@googlemail.com>
2020-06-28 17:45:55 +0200

    added autofac configuration module

---
+++ EntityComponentSystem/ECSModule.cs
@@ -0,0 +1,27 @@
+﻿using Autofac;
+using EntityComponentSystem.Caching;
+using EntityComponentSystem.Storages;
+using System.Diagnostics.CodeAnalysis;
+
+namespace EntityComponentSystem
+{
+  [ExcludeFromCodeCoverage]
+  public class ECSModule : Module
+  {
+    protected override void Load(ContainerBuilder builder)
+    {
+      builder.RegisterType<Context>()
+             .As<IContext>()
+             .SingleInstance();
+      builder.RegisterType<CacheManager>()
+             .As<ICacheManager>()
+             .SingleInstance();
+      builder.RegisterType<StorageManager>()
+             .As<IStorageManager>()
+             .SingleInstance();
+      builder.RegisterType<StorageFactory>()
+             .As<IStorageFactory>()
+             .SingleInstance();
+    }
+  }
+}
