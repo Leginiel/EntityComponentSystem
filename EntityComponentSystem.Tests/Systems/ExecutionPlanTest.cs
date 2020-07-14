72aefb2bdd5863d2039573d280e6b7fd129d7cde
Sebastian Berning <hunter.Vasitas@googlemail.com>
2020-07-14 20:20:03 +0200

    added execution plan class

---
+++ EntityComponentSystem.Tests/Systems/ExecutionPlanTest.cs
@@ -0,0 +1,65 @@
+﻿using EntityComponentSystem.Systems;
+using Moq;
+using Xunit;
+
+namespace EntityComponentSystem.Tests.Systems
+{
+  public class ExecutionPlanTest
+  {
+    [Fact]
+    public void TestAddSystem_ValidNotExistingSystem_SucecssfullyAdded()
+    {
+      Mock<ISystem> systemMock = new Mock<ISystem>();
+      ExecutionPlan executionPlan = new ExecutionPlan();
+
+      executionPlan.AddSystem(systemMock.Object);
+
+      Assert.Single(executionPlan.Systems);
+      Assert.Contains(systemMock.Object, executionPlan.Systems);
+    }
+    [Fact]
+    public void TestAddSystem_ValidExistingSystem_SucecssfullyAdded()
+    {
+      Mock<ISystem> systemMock = new Mock<ISystem>();
+      ExecutionPlan executionPlan = new ExecutionPlan();
+
+      executionPlan.AddSystem(systemMock.Object);
+      executionPlan.AddSystem(systemMock.Object);
+
+      Assert.Single(executionPlan.Systems);
+      Assert.Contains(systemMock.Object, executionPlan.Systems);
+    }
+    [Fact]
+    public void TestRemoveSystem_ValidExistingSystem_SuccessfullyRemoved()
+    {
+      Mock<ISystem> systemMock = new Mock<ISystem>();
+      ExecutionPlan executionPlan = new ExecutionPlan();
+
+      executionPlan.AddSystem(systemMock.Object);
+      executionPlan.RemoveSystem(systemMock.Object);
+
+      Assert.Empty(executionPlan.Systems);
+    }
+    [Fact]
+    public void TestRemoveSystem_NonaddedSystem_NoError()
+    {
+      Mock<ISystem> systemMock = new Mock<ISystem>();
+      ExecutionPlan executionPlan = new ExecutionPlan();
+
+      executionPlan.RemoveSystem(systemMock.Object);
+
+      Assert.Empty(executionPlan.Systems);
+    }
+    [Fact]
+    public void TestContainsSystem_ValidSystem_SucecssfullyAdded()
+    {
+      Mock<ISystem> systemMock = new Mock<ISystem>();
+      ExecutionPlan executionPlan = new ExecutionPlan();
+      bool result;
+      executionPlan.AddSystem(systemMock.Object);
+      result = executionPlan.Contains(systemMock.Object);
+
+      Assert.True(result);
+    }
+  }
+}
---
+++ EntityComponentSystem/Systems/IExecutionPlan.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace EntityComponentSystem.Systems
+{
+  public interface IExecutionPlan
+  {
+    List<ISystem> Systems { get; }
+
+    void AddSystem(ISystem system);
+    bool Contains(ISystem system);
+    void RemoveAllSystems();
+    void RemoveSystem(ISystem system);
+  }
+}
